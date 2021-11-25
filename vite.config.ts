2a7a218810194a585d76ee98a933dd2ed4401d74
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2021-11-25 18:04:12 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			external: ['three'],
+			output: {
+				paths: {
+					three: 'https://unpkg.com/three@0.134.0/build/three.min.js',
+				},
+			},
 		},
 	},
 });
